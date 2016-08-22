ebd6f3e098b2e21cfdeba29651de52681b00b0a5
umeda <umeda@team-lab.com>
2016-08-22 14:40:09 +0900

    First commit.

---
+++ Assets/SimpleComputeShader_Texture.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+public class SimpleComputeShader_Texture : MonoBehaviour
+{
+    public GameObject plane_A;
+    public GameObject plane_B;
+
+    public ComputeShader computeShader;
+
+    RenderTexture renderTexture_A;
+    RenderTexture renderTexture_B;
+
+    int kernelIndex_KernelFunction_A;
+    int kernelIndex_KernelFunction_B;
+
+    struct KernelSize
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public KernelSize(uint x, uint y, uint z)
+        {
+            this.x = (int)x;
+            this.y = (int)y;
+            this.z = (int)z;
+        }
+    }
+
+    KernelSize kernelSize_KernelFunction_A;
+    KernelSize kernelSize_KernelFunction_B;
+
+    void Start()
+    {
+        // enableRandomWrite を有効にして、
+        // 書き込み可能な状態のテクスチャを生成することが重要です。
+
+        this.renderTexture_A = new RenderTexture(512, 512, 0, RenderTextureFormat.ARGB32);
+        this.renderTexture_B = new RenderTexture(512, 512, 0, RenderTextureFormat.ARGB32);
+
+        this.renderTexture_A.enableRandomWrite = true;
+        this.renderTexture_B.enableRandomWrite = true;
+
+        this.renderTexture_A.Create();
+        this.renderTexture_B.Create();
+
+        // (1) カーネルのインデックスと、スレッドのサイズを保存します。
+
+        this.kernelIndex_KernelFunction_A = this.computeShader.FindKernel("KernelFunction_A");
+        this.kernelIndex_KernelFunction_B = this.computeShader.FindKernel("KernelFunction_B");
+
+        uint kernelSizeX, kernelSizeY, kernelSizeZ;
+
+        this.computeShader.GetKernelThreadGroupSizes
+            (this.kernelIndex_KernelFunction_A, out kernelSizeX, out kernelSizeY, out kernelSizeZ);
+
+        this.kernelSize_KernelFunction_A = new KernelSize(kernelSizeX, kernelSizeY, kernelSizeZ);
+
+        this.computeShader.GetKernelThreadGroupSizes
+            (this.kernelIndex_KernelFunction_B, out kernelSizeX, out kernelSizeY, out kernelSizeZ);
+
+        this.kernelSize_KernelFunction_B = new KernelSize(kernelSizeX, kernelSizeY, kernelSizeZ);
+
+        // ComputeShader で計算した結果を保存するためのバッファとして、ここではテクスチャを設定します。
+
+        this.computeShader.SetTexture
+            (this.kernelIndex_KernelFunction_A, "textureBuffer", this.renderTexture_A);
+        this.computeShader.SetTexture
+            (this.kernelIndex_KernelFunction_B, "textureBuffer", this.renderTexture_B);
+    }
+
+    void Update()
+    {
+        // Dispatch メソッドで ComputeShader を実行します。
+        // ここで、グループ数は、"解像度 / スレッド数" で算出します。
+        // これですべてのピクセルが処理されます。
+        // 
+        // 例えば 512 / 8 = 64 です。各グループは 64 ピクセルずつ平行に処理します。
+        // またグループスレッドは、64 / 8 = 8 ピクセルずつ並行に処理します。
+
+        this.computeShader.Dispatch(this.kernelIndex_KernelFunction_A,
+                                    this.renderTexture_A.width / this.kernelSize_KernelFunction_A.x,
+                                    this.renderTexture_A.height / this.kernelSize_KernelFunction_A.y,
+                                    this.kernelSize_KernelFunction_A.z);
+
+        this.computeShader.Dispatch(this.kernelIndex_KernelFunction_B,
+                                    this.renderTexture_B.width / this.kernelSize_KernelFunction_B.x,
+                                    this.renderTexture_B.height / this.kernelSize_KernelFunction_B.y,
+                                    this.kernelSize_KernelFunction_B.z);
+
+        // 実行して得られた結果をテクスチャとして設定します。
+
+        plane_A.GetComponent<Renderer>().material.mainTexture = this.renderTexture_A;
+        plane_B.GetComponent<Renderer>().material.mainTexture = this.renderTexture_B;
+    }
+}
